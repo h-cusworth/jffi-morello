36296961a2a6cbe00f37299f1c466439b6fc3605
Hannah Cusworth <cusworthh@thehutgroup.com>
2023-11-14 11:18:07 +0000

    Build succeeds. Note that not actually building libffi using system libffi instead.

--- cheri-build.sh
+++ cheri-build.sh
@@ -3,5 +3,6 @@
 export JAVA_HOME=/usr/local64/openjdk17
 export CC=clang-morello
 export CFLAGS="-march=morello+c64 -mabi=purecap"
+export LDFLAGS=""
 
-ant jar && ant archive-platform-jar && mvn package
+ant -Duse.system.libffi=1 jar && ant -Duse.system.libffi=1 archive-platform-jar && mvn package
