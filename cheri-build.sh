017d671a21d726a12894434bc42523868f0a3f53
Hannah Cusworth <cusworthh@thehutgroup.com>
2023-11-14 11:55:58 +0000

    tidy up - build can be configured without changing the makefile.

--- cheri-build.sh
+++ cheri-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export JAVA_HOME=/usr/local64/openjdk17
+export CFLAGS="-march=morello+c64 -mabi=purecap -I/usr/local/include"
 export CC=clang-morello
-export CFLAGS="-march=morello+c64 -mabi=purecap"
-export LDFLAGS=""
+export LDFLAGS="-mabi=purecap -fuse-ld=lld -L/usr/local/lib -lffi"
 
 ant -Duse.system.libffi=1 jar && ant -Duse.system.libffi=1 archive-platform-jar && mvn package
